ff47e5cd0580ab4e97d1555553ce0c577d8c5241
Mohammed Ayman <mohammedaymanaymu@gmail.com>
2025-12-09 21:51:05 +0530

    Initial setup: Created class structure and libraries

---
+++ page_replacement.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <iomanip>
+#include <algorithm>
+
+using namespace std;
+
+class PageReplacementSimulator {
+private:
+    vector<int> pages;
+    int numFrames;
+
+public:
+    void input() {}
+    void runFIFO() {}
+    void runLRU() {}
+    void runOptimal() {}
+};
+
+int main() {
+    cout << "OS Project Initialized..." << endl;
+    return 0;
+}
