633e1aab9aef7a96ce8b1516e4eb9ca75e3cdfb5
Aswin Pavithran <maswin715419@gmail.com>
2025-12-09 21:59:24 +0530

    Added input function for reading frames and reference string  C++
    Implemented input method to gather frame and page data.

--- page_replacement.cpp
+++ page_replacement.cpp
@@ -11,7 +11,19 @@ private:
     int numFrames;
 
 public:
-    void input() {}
+    void input() {
+        int n, val;
+        cout << "\n=== Input Configuration ===\n";
+        cout << "Enter number of Frames: ";
+        cin >> numFrames;
+        cout << "Enter number of Pages in reference string: ";
+        cin >> n;
+        cout << "Enter the reference string (space-separated): ";
+        for (int i = 0; i < n; i++) {
+            cin >> val;
+            pages.push_back(val);
+        }
+    }
     void runFIFO() {}
     void runLRU() {}
     void runOptimal() {}
